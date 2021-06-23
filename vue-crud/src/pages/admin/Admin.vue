bf7d89e4f89381c5ced196e7fc2dc0b88ca6df24
clarissaaaa <indriyaniclarissa@gmail.com>
2021-06-23 21:40:24 +0700

    Created vue user interfaces

---
+++ vue-crud/src/pages/admin/Admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <Nav />
+
+  <div class="container-fluid">
+    <div class="row">
+      <Menu />
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "../../components/Nav.vue";
+import Menu from "../../components/Menu.vue";
+export default {
+  components: { Nav, Menu },
+  name: "Admin",
+};
+</script>
