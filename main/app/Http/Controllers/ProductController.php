a5c3b00050f57fa2b13d153d66bd97aaadeb3871
clarissaaaa <indriyaniclarissa@gmail.com>
2021-06-24 11:55:20 +0700

    Created internal HTTP requests between microservices

--- main/app/Http/Controllers/ProductController.php
+++ main/app/Http/Controllers/ProductController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\ProductUser;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     public function index() 
     {
         return Product::all();
+    }
+
+    public function like($id, Request $request)
+    {
+        $response = \Http::get('http://host.docker.internal:8000/api/user');
+
+        $user = $response->json();
+
+        try{
+            ProductUser::create([
+                'user_id' => $user['id'],
+                'product_id' => $id
+            ]);
+
+            return response([
+                'messsage' => 'success'
+            ]);
+
+        }catch (\Exception $exception) {
+            return response([
+                'error' => 'You already liked this product!'
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
--- main/routes/api.php
+++ main/routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('products', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::post('products/{id}/like', [\App\Http\Controllers\ProductController::class, 'like']);
